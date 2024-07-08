86d44982a8ee0d303e960652a4f1cc85a2c0bdb0
Sylvain <scheveny@gmail.com>
2024-07-08 14:57:28 +0200

    création des classes

---
+++ DomainModel/Bottle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class Bottle
+    {
+        public int BottleID { get; set; }
+        public string BottleName { get; set; }
+        public int BottleYear { get; set; }
+        public string WineColor { get; set; }
+        public string Appellation { get; set; }
+        public int StorageStartYear { get; set; }
+        public int StorageEndYear { get; set; }
+        public int DrawerNb { get; set; }
+        public int StackInDrawerNb { get; set; }
+        public string PeakYears { get; set; }
+    }
+}
---
+++ DomainModel/Cellar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class Cellar
+    {
+        public int CellarID { get; set; }
+        public required string CellarName { get; set; }
+        public int NbRow { get; set; }
+        public int NbStackRow { get; set; }
+    }
+}
