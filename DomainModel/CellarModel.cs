b30c7c4fdf29245685c860610e921d98fd735d45
Sylvain <scheveny@gmail.com>
2024-07-08 15:35:15 +0200

    ajout des classes categorie et modele

---
+++ DomainModel/CellarModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class CellarModel
+    {
+        public int CellardModelID { get; set; }
+        public string CellarBrand { get; set; }
+        public int CellarTemperature { get; set; }
+    }
+}
