f5bb954d559f4026ed363accd40e838574b35f4b
furiousfab <fab4web@protonmail.com>
2024-07-15 09:15:59 +0200

    Lundi_bckp

--- Dal/Migrations/20240714052227_InitCellarWthCltLocal.Designer.cs
+++ Dal/Migrations/20240714052227_InitCellarWthCltLocal.Designer.cs
@@ -4,6 +4,7 @@ using Dal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dal.Migrations
 {
     [DbContext(typeof(CellarContext))]
-    partial class CellarContextModelSnapshot : ModelSnapshot
+    [Migration("20240714052227_InitCellarWthCltLocal")]
+    partial class InitCellarWthCltLocal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
